233f472511b1fe78c39d3c1071694234118b71c2
EmmaGoodliffe <emma@piemag.com>
2020-05-24 08:07:04 +0000

    refactored slightly

---
+++ index.py
@@ -0,0 +1,18 @@
+from wires import setup_wires
+from pygame import mixer
+
+# Music
+mixer.init()
+mixer.music.set_volume(0.2)
+
+# Wires
+WIRES = [4, 5, 6, 7, 8, 9]
+WIRES_TARGET = [5, 6, 8]
+def wires_success():
+    print(':)')
+def wires_error():
+    print(':(')
+def wires_progress():
+    mixer.music.load('sound/wires/progress.mp3')
+    mixer.music.play()
+setup_wires(WIRES, WIRES_TARGET, wires_success, wires_error, wires_progress)
--- wires.py
+++ wires.py
@@ -1,11 +1,5 @@
 from _74hc595 import get_controls
 from gpiozero import Button
-from pygame import mixer
-
-mixer.init()
-mixer.music.load('KTANE_wires_progress.mp3')
-mixer.music.set_volume(0.2)
-mixer.music.play()
 
 def get_pin_num(pin_str):
     return int(str(pin_str)[4:])
@@ -32,14 +26,3 @@ def setup_wires(WIRES, TARGET, success, error, progress):
 
     for button in buttons:
         button.when_released = check_wires
-
-# Run
-WIRES = [4, 5, 6, 7, 8, 9]
-TARGET = [5, 6, 8]
-def success():
-    print(':)')
-def error():
-    print(':(')
-def progress():
-    print('...')
-setup_wires(WIRES, TARGET, success, error, progress)
